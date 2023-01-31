a26d8b2afa22e394014acb49d1ada5dc89fd561d
cadypridgeon <cadypridgeon@gmail.com>
2023-01-31 02:06:34 -0600

    Finished desktop home page

---
+++ js/home.js
@@ -0,0 +1,44 @@
+// Credits: HackMIT 
+// https://hackmit.org/
+
+function open(question) {
+    let flower = question.parentElement.firstElementChild;
+    let answer = question.parentElement.nextElementSibling;
+
+    flower.style.animation = "spin 0.5s linear";
+    question.classList.add("clicked");
+    answer.style.maxHeight = answer.scrollHeight + "px";
+}
+
+function close(question) {
+    let flower = question.parentElement.firstElementChild;
+    let answer = question.parentElement.nextElementSibling;
+
+    flower.style.animation = "spin-back 0.5s linear";
+    question.classList.remove("clicked");
+    answer.style.maxHeight = "0";
+}
+
+let flowers = document.getElementsByClassName('question');
+
+for (let i = 0; i < flowers.length; i++) {
+    flowers[i].addEventListener("keypress", function (e) {
+        if (e.key === "Enter") {
+            flowers[i].click();
+        }
+    });
+    flowers[i].addEventListener("click", function (e) {
+        if (!flowers[i].classList.contains("clicked")) {
+            open(flowers[i]);
+
+            for (let j = 0; j < flowers.length; j++) {
+                if (i !== j && flowers[j].classList.contains("clicked")) {
+                    close(flowers[j]);
+                }
+            }
+
+        } else {
+            close(flowers[i]);
+        }
+    })
+}
